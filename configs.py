76c0d8f59101329b9a8dcde6db5f5882f8d9b65a
Jnanesh <99884442+Jnanesh124@users.noreply.github.com>
2024-07-07 21:35:44 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -4,12 +4,12 @@ import os
 
 
 class Config(object):
-    API_ID = int(os.environ.get("API_ID", 12345))
-    API_HASH = os.environ.get("API_HASH", "")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+    API_ID = int(os.environ.get("API_ID", 21942125))
+    API_HASH = os.environ.get("API_HASH", "6d412af77ce89f5bb1ed8971589d61b5")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6926166118:AAGmtLftMM4KTNLrZyTb9kRNHUvU-pSWquM")
     BOT_SESSION_NAME = os.environ.get("BOT_SESSION_NAME", "MdiskSearchBot")
-    USER_SESSION_STRING = os.environ.get("USER_SESSION_STRING", "")
-    CHANNEL_ID = int(os.environ.get("CHANNEL_ID", -100))
+    USER_SESSION_STRING = os.environ.get("USER_SESSION_STRING", "1BVtsOIYBu38qyIAP0zpeORw5SAl9IlY7PbwiUVPSsOJCNG3ll_UYFvQH9pBv_8XpegWPIpfHy8CSn18kKuzJDxSZ02GxOiioB9SRQdRWV-jSPNcvZjF-l7v_Lf_Cswjjd33Hx67YaLYXdRp2lbUoE2VHblnDreaI_jeimAeoJr3wNHxYg9FcYRyR2Zr8fd7nGx8pJwkDL1dIJOpWMlMYjyBA4yGq2zd6Sdl-joJJp8ZwtTPs_Z9KfgTTlRk4dmKcw6sHeRT8xdPm1SBgKm7GZHepUvtmKBCXdbyMBL96THlNVNDfJOH1wcKOh8dMvm1q1WoUJMoxMHA5Byp1Tg5kd6mVrOCb5SM=")
+    CHANNEL_ID = int(os.environ.get("CHANNEL_ID", -1001953742196))
     BOT_USERNAME = os.environ.get("BOT_USERNAME")
     BOT_OWNER = int(os.environ.get("BOT_OWNER"))
     UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", None)
