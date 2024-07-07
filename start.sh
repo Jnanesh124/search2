997c87aef5d0a00b30a208fe095e6c3f1832229a
Jnanesh <99884442+Jnanesh124@users.noreply.github.com>
2024-07-07 21:32:46 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo...."
-git clone https://github.com/RoyalKrrishna/MdiskWalaBot.git /MdiskWalaBot
-cd /MdiskWalaBot
+git clone https://github.com/Jnanesh124/search2.git /search2
+cd /search2
 pip3 install -r requirements.txt
 echo "Starting Bot...."
 python3 main.py
